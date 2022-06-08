17a2fc98dc2a849e83509045c4bfab064137de31
cherced <Chernandezcediel@gmail.com>
2022-06-08 14:08:35 -0500

    behivor of the chart, with animation

--- components/molecules/ContentDash.jsx
+++ components/molecules/ContentDash.jsx
@@ -1,31 +1,29 @@
 import React from "react";
 import { TimeLines } from "../atoms/TimeLines";
 
-
-
-
 export const ContentDash = () => {
+	const [toggleClass, setToggleClass] = React.useState(false);
+
   return (
     <main className="contentcontainer">
 			<div className="contentwrapper">
-				<div className="tabs">
+				<button onClick={() => setToggleClass(!toggleClass)} className="tabs">
 					<div className="categories">
 						<h2>Users</h2>
 					</div>
-				</div>
-				<div className="tabs">
+				</button>
+				<button className={toggleClass ? ("tabs") : ("tabs opaccity")}>
 					<div className="categories">
 						<h2>Bookings</h2>
 					</div>
-				</div>
-				<div className="tabs">
+				</button>
+				<button className={toggleClass ? ("tabs") : ("tabs opaccity")}>
 					<div className="categories">
 						<h2>Earnings</h2>
 					</div>
-				</div>
+				</button>
 			</div>
-			
-			<div className="charts">
+			<div className={ toggleClass ? ("charts") : ("charts show")}>
 				<div className="bar">
 					<h2>Users</h2>
 					<TimeLines />
